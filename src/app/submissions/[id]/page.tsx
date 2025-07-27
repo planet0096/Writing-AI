28f00e1bb97fba2d3b65cfd5cbced22c8a637edf
Gurpreet Singh <planet0096@gmail.com>
2025-07-27 11:47:51 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/submissions/[id]/page.tsx
+++ src/app/submissions/[id]/page.tsx
@@ -1,6 +1,7 @@
 
 "use client";
 
+import * as React from "react";
 import { useEffect, useState } from 'react';
 import { useParams, useRouter } from 'next/navigation';
 import { doc, getDoc, collection, query, where, getDocs } from 'firebase/firestore';
